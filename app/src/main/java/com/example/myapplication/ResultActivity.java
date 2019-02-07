d64bfc0a93a85a264cfa638bb7bca5ebb24a3e78
yeseoplee <l940712@gmail.com>
2019-02-07 23:59:36 +0900

    It works?!!?!?

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -13,6 +13,8 @@ public class ResultActivity extends AppCompatActivity {
 
     // 아 왜 안돼
 
+    //ㄱㄲ?
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
